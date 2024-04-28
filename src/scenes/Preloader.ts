e09f524a3c33b6d76293e36a38e508dff1b7fce8
DiametrFQ <hohlov.03@inbox.ru>
2024-04-28 22:45:05 +0300

    stupid vercel

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -3,15 +3,9 @@ import Map3 from "../../assets/Map3.json";
 import tiles from "@/scene.jpeg";
 import IMGPlayer from "@objects/Characters_aseprite.png";
 
+import PreloadGT from "./Preloaders/GameTools";
 import PreloadBubles from "./Preloaders/BublePreloader";
 import PreloadUI from "./Preloaders/UIPreloader";
-
-import wifi1 from "../../assets/gameTools/wi-fi1.png";
-import wifi2 from "@gameTools/wi-fi2.png";
-import wifi3 from "@gameTools/wi-fi3.png";
-import wifi4 from "@gameTools/wi-fi4.png";
-import keyboard from "@gameTools/keyboard_in_inventory.png";
-import HDM from "@gameTools/HDM_in_inventory.png";
 
 export class Preloader extends Phaser.Scene {
   constructor() {
@@ -19,12 +13,7 @@ export class Preloader extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("wifi1", wifi1);
-    this.load.image("wifi2", wifi2);
-    this.load.image("wifi3", wifi3);
-    this.load.image("wifi4", wifi4);
-    this.load.image("keyboard", keyboard);
-    this.load.image("HDM", HDM);
+    PreloadGT(this);
     PreloadBubles(this);
     PreloadUI(this);
 
